4694c56a1898f57b0b9d7f23f7986bb8e0f77de7
Michael Hirsch <scivision@users.noreply.github.com>
2022-09-13 17:34:28 -0400

    install cmake: add checks

--- scripts/install_cmake_linux.sh
+++ scripts/install_cmake_linux.sh
@@ -1,6 +1,8 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 # Installs CMake: Intel x86_64 Linux only
+
+set -e
 
 cd $HOME
 
@@ -8,7 +10,7 @@ version=3.24.2
 name=cmake-$version-linux-x86_64
 archive=$name.tar.gz
 
-if [[ $(uname -m) != "x86_64" ]]
+if [[ $(uname -m) != "x86_64" ]] || [[ "$OSTYPE" != "linux-gnu" ]]
 then
   echo "ERROR: this script is for Intel x86_64 Linux only"
   exit 1
